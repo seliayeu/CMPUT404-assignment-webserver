14124e45e3846852214f6b6d4bcd78aa9cbf6545
danila seliayeu <dan.seliayeu@gmail.com>
2021-09-24 13:02:42 -0600

    change citation to what was recommended in lecture in server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 from urllib.parse import unquote
 from email.utils import formatdate
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Danila Seliayeu
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,21 +29,6 @@ from email.utils import formatdate
 #
 # run: python freetests.py
 
-# try: curl -v -X GET http://127.0.0.1:8080/
-
-#   Copyright 2021 Danila Seliayeu
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
